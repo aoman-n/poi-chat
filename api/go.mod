147570c084551a2a956d9c61eb022697a2c918c9
aoba <ipbdipbd00@gmail.com>
2021-02-27 23:18:55 +0900

    Create api project

---
+++ api/go.mod
@@ -0,0 +1,10 @@
+module github.com/laster18/poi/api
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/rs/cors v1.7.0
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
---
+++ api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/laster18/poi/api/graph/generated"
+	"github.com/laster18/poi/api/src/resolvers"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	r := chi.NewRouter()
+
+    srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
+
+	r.Use(cors.Default().Handler)
+    r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+    r.Handle("/query", srv)
+
+    log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
