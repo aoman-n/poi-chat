4a19bb73e0791b24f65dead4216e09ab38a70fc4
aoba <ipbdipbd00@gmail.com>
2021-03-04 23:00:55 +0900

    [api] Add twitter api key to confg using .env file

--- api/go.mod
+++ api/go.mod
@@ -5,7 +5,9 @@ go 1.14
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/rs/cors v1.7.0
 	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
 )
--- api/src/config/config.go
+++ api/src/config/config.go
@@ -1,30 +1,36 @@
 package config
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
 type Environments struct {
-	Port          string `default:"8080"`
-	GoEnv         string `split_words:"true"`
-	DbUser        string `split_words:"true"`
-	DbName        string `split_words:"true"`
-	DbPassword    string `split_words:"true"`
-	DbHost        string `split_words:"true"`
-	DbPort        string `split_words:"true"`
-	RedisURL      string `split_words:"true"`
-	RedisPassowrd string `split_words:"true"`
+	Port             string `default:"8080"`
+	GoEnv            string `split_words:"true"`
+	DbUser           string `split_words:"true"`
+	DbName           string `split_words:"true"`
+	DbPassword       string `split_words:"true"`
+	DbHost           string `split_words:"true"`
+	DbPort           string `split_words:"true"`
+	RedisURL         string `split_words:"true"`
+	RedisPassowrd    string `split_words:"true"`
+	TwitterAPIKey    string `split_words:"true"`
+	TwitterSecretKey string `split_words:"true"`
 }
 
 var Env Environments
 
 func init() {
-	err := envconfig.Process("", &Env)
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("failed to read env variables err:", err)
+		log.Fatal("failed to load .env file, err:", err)
 	}
-	fmt.Printf("config: %#v", Env)
+
+	err = envconfig.Process("", &Env)
+	if err != nil {
+		log.Fatal("failed to read env variables,  err:", err)
+	}
 }
