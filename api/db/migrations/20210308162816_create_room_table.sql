c3f8ceba019131cedd24f603c826ce160ade1a9a
aoba <ipbdipbd00@gmail.com>
2021-03-09 01:52:04 +0900

    [api] Setup goose & Add rooms and messages table migration file

---
+++ api/db/migrations/20210308162816_create_room_table.sql
@@ -0,0 +1,15 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE rooms (
+  `id` int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `background_image_url` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE index_name_on_rooms (name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS rooms;
---
+++ api/db/migrations/20210308164040_create_message_table.sql
@@ -0,0 +1,20 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE messages (
+  `id` int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `room_id` int(11) UNSIGNED NOT NULL,
+  `body` text NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_avatar_url` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT messages_fk_room_id
+    FOREIGN KEY (`room_id`)
+    REFERENCES rooms (`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS messages;
