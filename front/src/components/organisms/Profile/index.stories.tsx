e2bc900e02781c11202ec855701eb970cb277879
aoba <ipbdipbd00@gmail.com>
2021-04-10 02:30:15 +0900

    [fornt] Create Profile component

---
+++ front/src/components/organisms/Profile/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import Profile, { ProfileProps } from '.'
+
+export default {
+  title: 'organisms/Profile',
+  component: Profile,
+} as Meta
+
+const Template: Story<ProfileProps> = (args) => (
+  <div className="w-64">
+    <Profile {...args} />
+  </div>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  profile: {
+    name: 'sample name',
+    avatarUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+  },
+}
---
+++ front/src/components/organisms/Profile/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type ProfileProps = {
+  profile: {
+    name: string
+    avatarUrl: string
+  }
+}
+
+const Profile: React.FC<ProfileProps> = ({ profile }) => {
+  return (
+    <div className="text-center py-6">
+      <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+        <img src={profile.avatarUrl} alt="my avatar" />
+      </div>
+      <div className="flex flex-col items-center text-center justify-center">
+        <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
+          {profile.name}
+        </h2>
+        <div className={`w-12 h-1 bg-gray-500 rounded mt-2 mb-4`}></div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
