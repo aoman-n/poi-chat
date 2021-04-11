f85bafd5a646ceacc4f40dcbcf967b6d80da85d0
aoba <ipbdipbd00@gmail.com>
2021-04-11 14:50:24 +0900

    [front] Change root path component

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,31 +1,42 @@
+import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.scss'
+import { NextPage } from 'next'
 
-export default function Home() {
+import MainTemplate from '@/components/templates/Main'
+import Header from '@/components/organisms/Header'
+import OnlineUserList from '@/components/organisms/OnlineUserList'
+import RoomList from '@/components/organisms/RoomList'
+import Profile from '@/components/organisms/Profile'
+
+import { mockOnlineUsers, mockRooms } from '@/mocks'
+
+const RoomsPageContainer: NextPage = () => {
+  return <RoomsPage />
+}
+
+const RoomsPage: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>ルーム一覧</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <Link href="/poi-room/sample">
-          <a>to Room</a>
-        </Link>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <MainTemplate
+        HeaderComponent={<Header isLoggedIn={true} />}
+        MainComponent={<RoomList rooms={mockRooms} />}
+        MyProfileComponent={
+          <Profile
+            profile={{
+              name: 'sample name',
+              avatarUrl:
+                'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+            }}
+          />
+        }
+        OnlineUserListComponent={<OnlineUserList users={mockOnlineUsers} />}
+      />
+    </>
   )
 }
+
+export default RoomsPageContainer
